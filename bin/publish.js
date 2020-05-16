972a40b891d759fe1316e42efca53e8214b505ab
yibingxiong <yibingxiong@126.com>
2020-05-16 10:32:33 +0800

    publish

--- bin/publish.js
+++ bin/publish.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const ROOT_PATH = path.join(__dirname, '..')
 const child_process = require('child_process')
+const copydir = require('copy-dir');
 
 child_process.execSync('git checkout gh-pages')
 child_process.execSync('git merge master')
@@ -15,8 +16,12 @@ const BOOK_PATH = path.join(ROOT_PATH, '_book')
 const dirs = fs.readdirSync(BOOK_PATH)
 for (let i = 0; i < dirs.length; i++) {
     const dir = dirs[i]
-    if (dir!=='bin') {
-        fs.copyFileSync(path.join(BOOK_PATH, dir), path.join(BOOK_PATH, '..', dir), fs.constants.COPYFILE_FICLONE);
+    if (dir !== 'bin') {
+        copydir.sync(path.join(BOOK_PATH, dir), path.join(BOOK_PATH, '..', dir), {
+            utimes: true,  // keep add time and modify time
+            mode: true,    // keep file mode
+            cover: true    // cover file when exists, default is true
+        });
     }
 }
 
